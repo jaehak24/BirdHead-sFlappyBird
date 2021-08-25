25329503aee733390c2da9cdc9d49cd544bbb014
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2021-08-26 04:56:07 +0900

    Flappy Bird
    동작은 전부 구현
    게임 저장, start 페이지 내일 구현

--- Assets/scripts/Bird.cs
+++ Assets/scripts/Bird.cs
@@ -8,24 +8,63 @@ public class Bird : MonoBehaviour {
     private const float JUMP_AMOUNT = 100f;
     private Rigidbody2D birdrigidbody2D;
 
+    private static Bird instance;
+
+    public static Bird GetInstance()
+    {
+        return instance;
+    }
+    public event EventHandler OnDied;
+    public event EventHandler OnStartedPlaying;
+    private State state;
+
+    private enum State
+    {
+        WaitingToStart,
+        Playing,
+        Dead,
+    }
     private void Awake()
     {
+        instance = this;
         birdrigidbody2D = GetComponent<Rigidbody2D>();
+        birdrigidbody2D.bodyType = RigidbodyType2D.Static;
+        state = State.WaitingToStart; // wake 단계에서 시작하기를 기다림
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)||Input.GetMouseButtonDown(0)) //마우스 스페이스바 둘다 가능
+        switch (state)
         {
-            Jump();
+            default:
+            case State.WaitingToStart:
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //마우스 스페이스바 둘다 가능
+                {
+                    state = State.Playing;// 스페이스바나, 마우스 클릭을 할 시 게임을 playing 상태로 변경
+                    birdrigidbody2D.bodyType = RigidbodyType2D.Dynamic; //dynamic 상태로 변경해줘야 함
+                    Jump();
+                    if (OnStartedPlaying != null) OnStartedPlaying(this, EventArgs.Empty);
+                }
+                break;
+            case State.Playing:
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //마우스 스페이스바 둘다 가능
+                {
+                    Jump();
+                }
+                break;
+            case State.Dead:
+                break;
         }
+        
     }
     private void Jump() {
         birdrigidbody2D.velocity=Vector2.up*JUMP_AMOUNT;// 해당 벡터에 점프 어만트만큼 올라감
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        CMDebug.TextPopupMouse("Dead!");
+        
+        birdrigidbody2D.bodyType = RigidbodyType2D.Static;
+        if (OnDied != null) OnDied(this, EventArgs.Empty);
     }
 
 }
--- Assets/scripts/Levels.cs
+++ Assets/scripts/Levels.cs
@@ -1,38 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey;
 
 public class Levels : MonoBehaviour
 {
     private const float CAMERA_ORTHO_SIZE = 50f;
     private const float PIPE_WITDH = 7.8f;
     private const float PIPE_HEAD_HEIGHT = 3.75f;
-    private const float PIPE_MOVE_SPEED = 10f;
+    private const float PIPE_MOVE_SPEED = 30f;
     private const float PIPE_DESTROY_X_POSITION=-100f;
     private const float PIPE_SPAWN_X_POSITION = +100f;
+    private const float BIRD_X_POSTION = 0f;
+
+    private static Levels instance;// 다른 스크립트에서 호출할 때 사용할 인스턴스
+    public static Levels GetInstance()
+    {
+        return instance;
+    }
 
     private List<Pipe> pipeList;
-
+    private int pipesPassedCount;
+    private int pipeSpawned; // 파이프가 생산된 횟수
     private float PipeSpawnTimer;
     private float PipeSpawnTimerMax;
+    private float gapSize;
+    private State state;
 
-
+    //난이도 조절
+    public enum Difficulty
+    {
+        Easy, 
+        Medium,
+        Hard,
+        Impossible,
+        Hell,
+    }
+    public enum State
+    {
+        WaitingToStart,
+        Playing,
+        birdDead,
+    }
     private void Awake()
     {
+        instance = this;
         pipeList = new List<Pipe>();
-        PipeSpawnTimerMax = .5f;//0.5초마다 생산되게끔 함
+        PipeSpawnTimerMax = 1f;//1초마다 생산되게끔 함
+        SetDifficulty(Difficulty.Easy);
+        state = State.WaitingToStart; 
     }
     private void Start()
     {
-        CreatePipe(50f, 20f,true); //카메라 (-50f0,0) 기준으로 높이, x좌표
-        CreatePipe(50f, 20f, false);
-        CreateGapPipes(50f, 20f, 40f); 
+        //새가 죽었을 때의 event 처리
+        Bird.GetInstance().OnDied += Bird_OnDied;
+        Bird.GetInstance().OnStartedPlaying += Level_OnStartedPlaying;
     }
+    private void Level_OnStartedPlaying(object sender, System.EventArgs e)
+    {
+        state = State.Playing;
+    }
+    private void Bird_OnDied(object sender, System.EventArgs e)
+    {
+        //CMDebug.TextPopupMouse("Dead!");
+        state = State.birdDead;
+
+        FunctionTimer.Create(() => {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");}, 1f); // 액션을 모노비헤이비어로 갖고 오는 커스텀 함수
+    } // 1초 후에 게임 다시 시작
 
     private void Update()
     {
-        HandlePipeMovement();
-        HandlePipeSpawning();
+        if (state == State.Playing)
+        {
+            HandlePipeMovement();
+            HandlePipeSpawning();
+        }
+               
     }
     private void HandlePipeSpawning()
     {
@@ -41,7 +85,13 @@ public class Levels : MonoBehaviour
         {
             //0보다 작으면 파이프를 하나 더 추가해랴 할 때
             PipeSpawnTimer += PipeSpawnTimerMax;//파이프 스폰타임 초기화
-            CreateGapPipes(50f, 20f, PIPE_SPAWN_X_POSITION); // 가장 최초 위치서부터 파이프들이 생성됨
+            float HeightEdgeLimit = 10f;//갭에다가 더할 최소 가중치
+            float minHeight = gapSize *.5f+ HeightEdgeLimit;//최소 사이즈=최소 지정 사이즈+여백+1/2
+            float TotlaHeight = CAMERA_ORTHO_SIZE * 2f;
+            float MaxHegiht = TotlaHeight - gapSize * .5f-HeightEdgeLimit;
+            float height = Random.Range(minHeight, MaxHegiht);
+            CreateGapPipes(height, gapSize, PIPE_SPAWN_X_POSITION); // 가장 최초 위치서부터 파이프들이 생성됨
+            
 
         }
 
@@ -52,8 +102,14 @@ public class Levels : MonoBehaviour
         {
            {
                Pipe pipe = pipeList[i];
-               //파이프를 파이프 속도에 따라 왼쪽으로 이동
-               pipe.Move();
+                //파이프를 파이프 속도에 따라 왼쪽으로 이동
+                bool isTotheRightoftheBird = pipe.GetXPosition() > BIRD_X_POSTION;
+                pipe.Move();
+                if(isTotheRightoftheBird&&pipe.GetXPosition() <= BIRD_X_POSTION&&pipe.IsBottom())
+                {
+                    //bird pass the pipe
+                    pipesPassedCount++;
+                }
                //리소스를 잡아 먹지 않기 위해 카메라 앵글을 벗어나면 파이프를 제거하는 것
                if (pipe.GetXPosition() < PIPE_DESTROY_X_POSITION)
                {
@@ -65,21 +121,61 @@ public class Levels : MonoBehaviour
            }
 
         }
-        
+    }
+    private void SetDifficulty(Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                gapSize = 50f;
+                PipeSpawnTimerMax = 1.2f;
+                break;
+            case Difficulty.Medium:
+                gapSize = 40f;
+                PipeSpawnTimerMax = 1.1f;
+                break;
+            case Difficulty.Hard:
+                gapSize = 30f;
+                Debug.Log("Hard");
+                PipeSpawnTimerMax = 1.0f;
+                break;
+            case Difficulty.Impossible:                
+                gapSize = 23f;
+                PipeSpawnTimerMax = 0.9f;
+                Debug.Log("Impossible");
+                break;
+            case Difficulty.Hell:
+                gapSize = 19f;
+                PipeSpawnTimerMax = 0.85f;
+                Debug.Log("HEll");
+                break;
+        }
+    }
+    private Difficulty GetDifficulty()
+    {
+        if (pipeSpawned >= 40) return Difficulty.Hell;
+        if (pipeSpawned >= 30) return Difficulty.Impossible;
+        if (pipeSpawned >= 20) return Difficulty.Hard;
+        if (pipeSpawned >= 10) return Difficulty.Medium;
+        else
+            return Difficulty.Easy;
+
 
     }
     private void CreateGapPipes(float gapY, float gapSize, float xPosition)
     {
         CreatePipe(gapY-gapSize*.5f,xPosition,true);//아래쪽 파이프
         CreatePipe(CAMERA_ORTHO_SIZE * 2f - gapY - gapSize * .5f, xPosition, false);
+        pipeSpawned++;
+        SetDifficulty(GetDifficulty()); // 파이프를 소환할 때마다 setDifficulty를 통해서 난이도를 확인
     }
-    private void CreatePipe(float height, float xPosition, bool createBottom)//하단 상단 파이프 빌드를 위한 bool 인자 추가
+    private void CreatePipe(float height, float xPosition, bool isBottom)//하단 상단 파이프 빌드를 위한 bool 인자 추가
     {
         //파이프 헤드 
         Transform pipeHead = Instantiate(GameAssets.GetInstance().pfPipeHead);
         float pipeheadPostition;
         //파이프 헤드 위치 판별(상하)
-        if (createBottom)
+        if (isBottom)
         {
             pipeheadPostition = -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_HEIGHT * .38f;
         }
@@ -91,7 +187,7 @@ public class Levels : MonoBehaviour
         //파이프 바디
         float bodyPosition;
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
-        if (createBottom)
+        if (isBottom)
             bodyPosition = -CAMERA_ORTHO_SIZE;
         else {
             bodyPosition = CAMERA_ORTHO_SIZE;
@@ -113,23 +209,32 @@ public class Levels : MonoBehaviour
         pipeBodyBoxCollider.size = new Vector2(PIPE_WITDH, height);
         pipeBodyBoxCollider.offset = new Vector2(0f, height * .38f);
 
-        Pipe pipe = new Pipe(pipeHead, pipeBody);// 새로운 파이프 선언
+        Pipe pipe = new Pipe(pipeHead, pipeBody, isBottom);// 새로운 파이프 선언
         pipeList.Add(pipe);
 
     }
-
+    public int GetPipeSpawned()
+    {
+        return pipeSpawned;
+    }
+    public int getPipespassedCount()
+    {
+        return pipesPassedCount;
+    }
     /*
-     *하나의 파이프를 가리킬 때
+     *싱글 파이프 클래스
      */
     private class Pipe
     {
         private Transform pipeHeadTransform;
         private Transform pipeBodyTransform;
+        private bool isBottom;
 
-        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform)
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform, bool isBottom)
         {
             this.pipeHeadTransform = pipeHeadTransform;
             this.pipeBodyTransform = pipeBodyTransform;
+            this.isBottom = isBottom;
         }
 
         public void Move() 
@@ -142,7 +247,10 @@ public class Levels : MonoBehaviour
         {
             return pipeHeadTransform.position.x;
         }
-
+        public bool IsBottom()
+        {
+            return isBottom;
+        }
         public void DestroySelf()
         {
             Destroy(pipeBodyTransform.gameObject);
